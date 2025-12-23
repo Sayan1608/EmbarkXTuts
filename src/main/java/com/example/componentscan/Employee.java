b23632a58d8923e635f9a5e5d1c2ee4c2ff7831d
sayan <subhosen16@gmail.com>
2025-12-23 23:33:52 +0530

    Component and Component Scan demo

---
+++ src/main/java/com/example/componentscan/Employee.java
@@ -0,0 +1,45 @@
+package com.example.componentscan;
+
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class Employee {
+    private Long empId;
+    private String empName;
+    private BigDecimal salary;
+
+    public Long getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(Long empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + empId +
+                ", empName='" + empName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
